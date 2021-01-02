0fab65f22e64253bb4137e8d5f3e619cc567673e
Emmanuel Opel <opegude.n3t@gmail.com>
2021-01-02 18:40:58 +0300

    corrected socket url

--- src/socketurls.js
+++ src/socketurls.js
@@ -3,12 +3,12 @@ export const socketurls = (function (){
 
     if (process.env.NODE_ENV === "development"){
      return "http://localhost:5000"
-      //return "https://odavoltadmin.herokuapp.com/" 
+      //return "https://odavoltapi.herokuapp.com/" 
     
     }
     else if(process.env.NODE_ENV === "production"){
 
-      return "https://odavoltadmin.herokuapp.com/"
+      return "https://odavoltapi.herokuapp.com/"
     }
       
     } ());
