9358003fc37ea255100662cd9ef40e4c46b323ce
Emmanuel Opel <opegude.n3t@gmail.com>
2021-01-06 14:54:10 +0300

    fixed all the cartMethod errors

--- src/components/home/Product.js
+++ src/components/home/Product.js
@@ -5,49 +5,7 @@ import AddToCartComponent from '../reusable/addtocart/AddToCartComponent'
 
 const Product=({item,manageAutoPlay})=> {
 
-    //redux
-    const {cartChange} = useSelector((state)=>state.cartReducer)
-   
-
-    const [itemInLocalStorage,SetItemInLocalStorage]= useState(null)
-    const [itemNotInLocalStorage,setItemNotInLocalStorage] = useState(item)
-    
-
- 
-      useEffect(() => {
-        IsinLocalStorage(item)
-        // eslint-disable-next-line   
-      },[cartChange])
- 
-    function IsinLocalStorage(item){
-        //find items from  local storage and set to currnet item
-        let lsItems = JSON.parse(localStorage.getItem("ov-client-orders"))
-        if(lsItems === null){
-            //remove the isinLocalStorage prop or set to undefined
-            let i = item
-           i.isinLocalStorage = undefined
-            setItemNotInLocalStorage(i)
-            SetItemInLocalStorage(null)
-            return null
-        }
-        else if ( lsItems.length === 0){
-            //remove the isinLocalStorage prop or set to undefined
-            let i = item
-            i.isinLocalStorage = undefined
-            setItemNotInLocalStorage(i)
-            SetItemInLocalStorage(null)
-            return null
-        }
-
-        lsItems.forEach((i)=>{
-            if(i._id === item._id){
-                SetItemInLocalStorage(i)
-            }
-          
-            
-        })
-        
-      }
+  
 
      return (
      <div className="ov-product-cont" key={item._id} onClick={manageAutoPlay} onMouseOver={manageAutoPlay} onDrag={manageAutoPlay} >
@@ -71,10 +29,10 @@ const Product=({item,manageAutoPlay})=> {
 
              {/* add to cart section */}
              <div className="ov-product-addtocart-cont">
-                   {
-                       itemInLocalStorage === null ? <AddToCartComponent item={itemNotInLocalStorage} /> :
-                       <AddToCartComponent item={itemInLocalStorage} />
-                   }
+                  
+                        <AddToCartComponent item={item} /> 
+                      
+  
              </div>
         
  </div>
