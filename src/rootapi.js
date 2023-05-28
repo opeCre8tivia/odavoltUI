bfd82721db98b2a0c82513a6406605a7a3709eee
opeCre8tivia <opegude.n3t@gmail.com>
2023-05-28 13:57:50 +0300

    change rootapi

--- src/rootapi.js
+++ src/rootapi.js
@@ -1,8 +1,8 @@
 export const rootapi = (function () {
   if (process.env.NODE_ENV === "development") {
-    return "http://localhost:5000";
+    return "http://localhost:5000"
     // return "https://odavoltapi.herokuapp.com"
   } else if (process.env.NODE_ENV === "production") {
-    return "https://odavoltapi.herokuapp.com";
+    return "https://odavolt-api.onrender.com"
   }
-})();
+})()
